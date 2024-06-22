b1b8766efcd6b871a349ab264c6477a2165e2422
Marian Pekár <marian.pekar@gmail.com>
2024-06-23 01:05:55 +0200

    Initial commit

---
+++ Source/Maze/MazeGenerator.cpp
@@ -0,0 +1,234 @@
+#include "MazeGenerator.h"
+#include "Math/Vector.h"
+
+AMazeGenerator::FMazeData::FMazeData(AMazeGenerator& MazeGen): MazeGen{MazeGen}
+{
+}
+
+void AMazeGenerator::FMazeData::Init()
+{
+	Data.Init(1, MazeGen.Width * MazeGen.Height);
+}
+
+int8 AMazeGenerator::FMazeData::Get(const int32& X, const int32& Y) const
+{
+	return Data[X + Y * MazeGen.Width];
+}
+
+void AMazeGenerator::FMazeData::Set(const int32& X, const int32& Y, const int8& Value)
+{
+	Data[X + Y * MazeGen.Width] = Value;
+}
+
+void AMazeGenerator::FMazeData::Open(const int32& X, const int32& Y)
+{
+	Set(X, Y, 0);
+}
+
+bool AMazeGenerator::FMazeData::IsOpen(const int32& X, const int32& Y) const
+{
+	return Data[X + Y * MazeGen.Width] == 0;
+}
+
+bool AMazeGenerator::FMazeData::IsValid(const int32& X, const int32& Y) const
+{
+	return X >= 0 && X < MazeGen.Width - 1 && Y >= 0 && Y < MazeGen.Height - 1;
+}
+
+void AMazeGenerator::FWorldDirections::Shuffle()
+{
+	const int32 LastIndex = Data.Num() - 1;
+	for (int32 i = 0; i <= LastIndex; ++i)
+	{
+		const int32 Index = FMath::RandRange(0, LastIndex);
+		if (i != Index)
+		{
+			Data.Swap(i, Index);
+		}
+	}
+}
+
+TTuple<int32, int32> AMazeGenerator::FWorldDirections::operator[](const size_t& Index)
+{
+	return Data[Index];
+}
+
+int32 AMazeGenerator::FWorldDirections::Num() const
+{
+	return Data.Num();
+}
+
+AMazeGenerator::AMazeGenerator()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+
+void AMazeGenerator::BeginPlay()
+{
+	Super::BeginPlay();
+
+	GenerateMaze();
+	PlacePieces();
+}
+
+void AMazeGenerator::GenerateMaze()
+{
+	Maze.Init();
+	Maze.Open(StartX, StartY);
+
+	switch (Algorithm) {
+	case EMazeGenerationAlgorithmType::WithStack:
+		GenerateMazeWithStack();
+		break;
+	case EMazeGenerationAlgorithmType::WithRecursion:
+		GenerateMazeWithRecursion(StartX, StartY);
+		break;
+	}
+
+}
+
+void AMazeGenerator::GenerateMazeWithStack()
+{
+	TArray<TTuple<int32, int32>> Stack;
+	Stack.Push({StartX, StartY});
+
+	while (!Stack.IsEmpty())
+	{
+		const int32 CurrentX = Stack.Top().Key;
+		const int32 CurrentY = Stack.Top().Value;
+		Stack.Pop();
+
+		Directions.Shuffle();
+
+		for (size_t i = 0; i < 4; ++i)
+		{
+			int32 NextX = CurrentX + Directions[i].Key * 2;
+			int32 NextY = CurrentY + Directions[i].Value * 2;
+
+			if (!Maze.IsValid(NextX, NextY))
+			{
+				continue;
+			}
+
+			if (!Maze.IsOpen(NextX, NextY))
+			{
+				Maze.Open((NextX + CurrentX) / 2, (NextY + CurrentY) / 2);
+				Maze.Open(NextX, NextY);
+				Stack.Push({NextX, NextY});
+			}
+		}
+	}
+}
+
+void AMazeGenerator::GenerateMazeWithRecursion(const int32& X, const int32& Y)
+{
+	Directions.Shuffle();
+
+	for (int32 i = 0; i < Directions.Num(); i++)
+	{
+		if (Directions[i] == TTuple<int32, int32>{1,0}) // Up
+		{
+			if (X + 2 >= Width - 1 || Maze.IsOpen(X + 2, Y))
+				continue;
+
+			Maze.Open(X + 2, Y);
+			Maze.Open(X + 1, Y);
+			GenerateMazeWithRecursion(X + 2, Y);
+		}
+		else if (Directions[i] == TTuple<int32, int32>{0,-1}) // Right
+		{
+			if (Y + 2 >= Height - 1 || Maze.IsOpen(X, Y + 2))
+				continue;
+
+			Maze.Open(X, Y + 2);
+			Maze.Open(X, Y + 1);
+			GenerateMazeWithRecursion(X, Y + 2);
+		}
+		else if (Directions[i] == TTuple<int32, int32>{-1,0}) // Down
+		{
+			if (X - 2 <= 0 || Maze.IsOpen(X - 2, Y))
+				continue;
+
+			Maze.Open(X - 2, Y);
+			Maze.Open(X - 1, Y);
+			GenerateMazeWithRecursion(X - 2, Y);
+		}
+		else if (Directions[i] == TTuple<int32, int32>{0,1}) // Left
+		{
+			if (Y - 2 <= 0 || Maze.IsOpen(X, Y - 2))
+				continue;
+
+			Maze.Open(X, Y - 2);
+			Maze.Open(X, Y - 1);
+			GenerateMazeWithRecursion(X, Y - 2);
+		}
+	}
+}
+
+void AMazeGenerator::PlacePieces() const
+{
+	for (int32 x = 1; x < Width - 1; x++)
+	{
+		for (int32 y = 1; y < Height - 1; y++)
+		{
+			// Straight
+			if (IsPatternMatching(x, y, HorizontalStraightPattern)) { PlacePiece(x, y, 90.f, StraightPiece); }
+			else if (IsPatternMatching(x, y, VerticalStraightPattern)) { PlacePiece(x, y, 0.f, StraightPiece); }
+
+			// Turns
+			else if (IsPatternMatching(x, y, TurnLeftUpPattern)) { PlacePiece(x, y, 0.f, TurnPiece); }
+			else if (IsPatternMatching(x, y, TurnLeftDownPattern)) { PlacePiece(x, y, 90.f, TurnPiece); }
+			else if (IsPatternMatching(x, y, TurnRightUpPattern)) { PlacePiece(x, y, -90.f, TurnPiece); }
+			else if (IsPatternMatching(x, y, TurnRightDownPattern)) { PlacePiece(x, y, 180.f, TurnPiece); }
+
+			// T Junctions
+			else if (IsPatternMatching(x, y, TJunctionUpPattern)) { PlacePiece(x, y, -90.f, TJunctionPiece); }
+			else if (IsPatternMatching(x, y, TJunctionDownPattern)) { PlacePiece(x, y, 90.f, TJunctionPiece); }
+			else if (IsPatternMatching(x, y, TJunctionLeftPattern)) { PlacePiece(x, y, 0.f, TJunctionPiece); }
+			else if (IsPatternMatching(x, y, TJunctionRightPattern)) { PlacePiece(x, y, 180.f, TJunctionPiece); }
+
+			// Dead ends
+			else if (IsPatternMatching(x, y, DeadEndUpPattern)) { PlacePiece(x, y, 90.f, DeadEndPiece); }
+			else if (IsPatternMatching(x, y, DeadEndDownPattern)) { PlacePiece(x, y, -90.f, DeadEndPiece); }
+			else if (IsPatternMatching(x, y, DeadEndLeftPattern)) { PlacePiece(x, y, 180.f, DeadEndPiece); }
+			else if (IsPatternMatching(x, y, DeadEndRightPattern)) { PlacePiece(x, y, 0.f, DeadEndPiece); }
+
+			// Crossroad
+			else if (IsPatternMatching(x, y, CrossroadPattern)) { PlacePiece(x, y, 0.f, CrossroadPiece); }
+		}
+	}
+}
+
+// All patterns are defined in Maze.h
+// Example pattern: [ 1 0 1
+//                    0 0 0
+//                    1 0 1 ] is a crossroad.
+bool AMazeGenerator::IsPatternMatching(const int32& X, const int32& Y, const TArray<int8>& Pattern) const
+{
+	int Count = 0;
+	int i = 0;
+	for (int y = 1; y > -2; y--)
+	{
+		for (int x = -1; x < 2; x++)
+		{
+			if (Pattern[i] == Maze.Get(X + x, Y + y) || Pattern[i] == 5)
+			{
+				Count++;
+			}
+
+			i++;
+		}
+	}
+
+	return Count == 9;
+}
+
+void AMazeGenerator::PlacePiece(const int32& X, const int32& Y, const float& Yaw, const TSubclassOf<AActor>& Piece) const
+{
+	const FVector Location(X * Scale, Y * Scale, 0);
+	const FRotator Rotation(0, Yaw, 0);
+	const FActorSpawnParameters SpawnInfo;
+
+	GetWorld()->SpawnActor<AActor>(Piece, Location, Rotation, SpawnInfo);
+}
---
+++ Source/Maze/MazeGenerator.h
@@ -0,0 +1,190 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MazeGenerator.generated.h"
+
+UENUM(BlueprintType)
+enum class EMazeGenerationAlgorithmType : uint8
+{
+	WithStack,
+	WithRecursion
+};
+
+UCLASS()
+class MAZE_API AMazeGenerator : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AMazeGenerator();
+
+protected:
+	virtual void BeginPlay() override;
+
+	class FMazeData
+	{
+	public:
+		FMazeData(AMazeGenerator& MazeGen);
+		void Init();
+		int8 Get(const int32& X, const int32& Y) const;
+		bool IsValid(const int32& X, const int32& Y) const;
+		void Open(const int32& X, const int32& Y);
+		bool IsOpen(const int32& X, const int32& Y) const;
+	private:
+		void Set(const int32& X, const int32& Y, const int8& Value);
+		AMazeGenerator& MazeGen;
+		TArray<int8> Data;
+	};
+
+	FMazeData Maze = FMazeData(*this);
+
+	class FWorldDirections
+	{
+	public:
+		void Shuffle();
+		TTuple<int32, int32> operator[](const size_t& Index);
+		int32 Num() const;
+	private:
+		TArray<TTuple<int32, int32>> Data = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+	};
+
+	FWorldDirections Directions;
+
+	void GenerateMaze();
+	void GenerateMazeWithStack();
+	void GenerateMazeWithRecursion(const int32& X, const int32& Y);
+
+	void PlacePieces() const;
+	void PlacePiece(const int32& X, const int32& Y, const float& Yaw, const TSubclassOf<AActor>& Piece) const;
+	bool IsPatternMatching(const int32& X, const int32& Y, const TArray<int8>& Pattern) const;
+
+	// Straight
+	TArray<int8> HorizontalStraightPattern = {
+		5, 1, 5,
+		0, 0, 0,
+		5, 1, 5
+	};
+
+	TArray<int8> VerticalStraightPattern = {
+		5, 0, 5,
+		1, 0, 1,
+		5, 0, 5
+	};
+
+	// T Junctions
+	TArray<int8> TJunctionUpPattern = {
+		1, 0, 1,
+		0, 0, 0,
+		5, 1, 5
+	};
+
+	TArray<int8> TJunctionDownPattern = {
+		5, 1, 5,
+		0, 0, 0,
+		1, 0, 1
+	};
+
+	TArray<int8> TJunctionLeftPattern = {
+		1, 0, 5,
+		0, 0, 1,
+		1, 0, 5
+	};
+
+	TArray<int8> TJunctionRightPattern = {
+		5, 0, 1,
+		1, 0, 0,
+		5, 0, 1
+	};
+
+	// Crossroad
+	TArray<int8> CrossroadPattern = {
+		1, 0, 1,
+		0, 0, 0,
+		1, 0, 1
+	};
+
+	// Turns
+	TArray<int8> TurnLeftUpPattern = {
+		1, 0, 5,
+		0, 0, 1,
+		5, 1, 5
+	};
+
+	TArray<int8> TurnLeftDownPattern = {
+		5, 1, 5,
+		0, 0, 1,
+		1, 0, 5
+	};
+
+	TArray<int8> TurnRightUpPattern = {
+		5, 0, 1,
+		1, 0, 0,
+		5, 1, 5
+	};
+
+	TArray<int8> TurnRightDownPattern = {
+		5, 1, 5,
+		1, 0, 0,
+		5, 0, 1
+	};
+
+	// Dead ends
+	TArray<int8> DeadEndUpPattern = {
+		5, 0, 5,
+		1, 0, 1,
+		5, 1, 5
+	};
+
+	TArray<int8> DeadEndDownPattern = {
+		5, 1, 5,
+		1, 0, 1,
+		5, 0, 5
+	};
+
+	TArray<int8> DeadEndLeftPattern = {
+		5, 1, 5,
+		0, 0, 1,
+		5, 1, 5
+	};
+
+	TArray<int8> DeadEndRightPattern = {
+		5, 1, 5,
+		1, 0, 0,
+		5, 1, 5
+	};
+
+public:
+	UPROPERTY(EditAnywhere)
+	EMazeGenerationAlgorithmType Algorithm = EMazeGenerationAlgorithmType::WithRecursion;
+
+	UPROPERTY(EditAnywhere)
+	int32 StartX = 5;
+
+	UPROPERTY(EditAnywhere)
+	int32 StartY = 5;
+
+	UPROPERTY(EditAnywhere)
+	int32 Width = 100;
+
+	UPROPERTY(EditAnywhere)
+	int32 Height = 50;
+
+	UPROPERTY(EditAnywhere)
+	int32 Scale = 300;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<AActor> StraightPiece;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<AActor> TJunctionPiece;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<AActor> CrossroadPiece;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<AActor> TurnPiece;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<AActor> DeadEndPiece;
+};
